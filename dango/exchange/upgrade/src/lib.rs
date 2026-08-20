1c9f97c401b0cff4ba2c1e091ea1365fbf0fd561
larry <26318510+larry0x@users.noreply.github.com>
2026-08-20 18:12:29 +0100

    chore: remove the already executed upgrade logic

--- dango/exchange/upgrade/src/lib.rs
+++ dango/exchange/upgrade/src/lib.rs
@@ -1,13 +1,10 @@
-mod bank;
-mod perps;
-
 use {
     dango_app::AppResult,
     dango_primitives::{BlockInfo, Storage},
 };
 
-pub fn do_upgrade<VM>(storage: Box<dyn Storage>, _vm: VM, _block: BlockInfo) -> AppResult<()> {
-    bank::do_bank_upgrades(storage)?;
+pub fn do_upgrade<VM>(_storage: Box<dyn Storage>, _vm: VM, _block: BlockInfo) -> AppResult<()> {
+    tracing::info!("Nothing to do in this upgrade");
 
     Ok(())
 }
